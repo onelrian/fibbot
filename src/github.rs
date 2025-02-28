adcd274675c16b9d06d1973614a79c00b6f9a01a
onelrian <onelsob57@gmail.com>
2025-02-28 10:12:52 +0100

    Add GitHub API interaction module (github.rs)

---
+++ src/github.rs
@@ -0,0 +1,26 @@
+use reqwest::Client;
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct Comment {
+    pub body: String,
+}
+
+pub async fn post_comment(pr_url: &str, token: &str, comment: &str) -> Result<(), reqwest::Error> {
+    let client = Client::new();
+    let api_url = format!("{}/comments", pr_url);
+    let res = client
+        .post(&api_url)
+        .bearer_auth(token)
+        .json(&Comment { body: comment.to_string() })
+        .send()
+        .await?;
+
+    if res.status().is_success() {
+        println!("Comment posted successfully!");
+    } else {
+        eprintln!("Failed to post comment: {}", res.status());
+    }
+
+    Ok(())
+}
